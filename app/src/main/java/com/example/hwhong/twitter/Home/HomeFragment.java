2364c92f6f253c76c0238f7766d4ff34ecc0d257
Hwa Chien Hong <hwhong1129@gmail.com>
2017-08-07 13:09:22 +0900

    cleanup

--- app/src/main/java/com/example/hwhong/twitter/Home/HomeFragment.java
+++ app/src/main/java/com/example/hwhong/twitter/Home/HomeFragment.java
@@ -1,30 +1,19 @@
 package com.example.hwhong.twitter.Home;
 
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.hwhong.twitter.R;
-import com.example.hwhong.twitter.test;
-import com.twitter.sdk.android.core.DefaultLogger;
 import com.twitter.sdk.android.core.Twitter;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
-import com.twitter.sdk.android.core.TwitterConfig;
 import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
 import com.twitter.sdk.android.tweetui.UserTimeline;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class HomeFragment extends Fragment  {
 
@@ -59,7 +48,7 @@ public class HomeFragment extends Fragment  {
         View view =  inflater.inflate(R.layout.fragment_home, container, false);
         ButterKnife.bind(this, view);
         final UserTimeline userTimeline = new UserTimeline.Builder()
-                .screenName("twitterdev")
+                .screenName("hwhong1129")
                 .build();
         final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(getContext())
                 .setTimeline(userTimeline)
