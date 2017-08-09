15b15d0edd0fef0488daaab1457a8e50d942d4b0
Hwa Chien Hong <hwhong1129@gmail.com>
2017-08-09 17:35:41 +0900

    need to fix the item selected color

--- app/src/main/java/com/example/hwhong/twitter/MainActivity.java
+++ app/src/main/java/com/example/hwhong/twitter/MainActivity.java
@@ -107,6 +107,7 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.nav_profile:
+                        navigationView.getMenu().getItem(0).setChecked(false);
                         startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
                         break;
                     default:
