01f0838760a4012fbc16598460f80ef0747dd318
Hwa Chien Hong <hwhong1129@gmail.com>
2017-08-23 10:55:11 +0800

    new files added for composition of tweets

---
+++ app/src/main/java/com/example/hwhong/twitter/Profile/TweetComposition.java
@@ -0,0 +1,15 @@
+package com.example.hwhong.twitter.Profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.hwhong.twitter.R;
+
+public class TweetComposition extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tweet_composition);
+    }
+}
